ecccb6bf11ad40297ab891454f1da6141f6fb749
aliawad93 <55746034+aliawad93@users.noreply.github.com>
2026-06-11 16:01:23 +0100

    Add Databricks orientation notebook

---
+++ notebooks/week_01/week_01_01_databricks_orientation.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+print("Hello Databricks")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello Databricks SQL" AS message;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Week 1 - Databricks Orientation
+# MAGIC
+# MAGIC ## What I learned today
+# MAGIC
+# MAGIC - I opened Databricks Free Edition.
+# MAGIC - I created my first notebook.
+# MAGIC - I ran a Python cell.
+# MAGIC - I ran a SQL cell using `%sql`.
+# MAGIC
+# MAGIC ## Key terms
+# MAGIC
+# MAGIC - Databricks workspace
+# MAGIC - Notebook
+# MAGIC - Python cell
+# MAGIC - SQL cell
+# MAGIC - Serverless compute
+# MAGIC
+# MAGIC ## Questions I still have
+# MAGIC
+# MAGIC - 
+
+# COMMAND ----------
+
